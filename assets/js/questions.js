e5f944d6469702ec95aa29ab03e650a4c5f9d0e8
jessica stephenson <jesslouisestephenson@gmail.com>
2023-01-16 14:52:51 +0000

    updated JS

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,27 +3,27 @@
 var questions = [
     {
         questions: "Inside which HTML element do we put the JavaScript?",
-        choicesEl: ["<script>", "<js>", "<scripting>", "<javascript>"],
+        choices: ["<script>", "<js>", "<scripting>", "<javascript>"],
         answer: "<script>",
     },
     {
         questions: "Where is the correct place to insert a JavaScript?",
-        choicesEl: ["the <head> section", "Both the <head> section and the <body> section are correct", "The <body> section"],
+        choices: ["the <head> section", "Both the <head> section and the <body> section are correct", "The <body> section"],
         answer: "Both the <head> section and the <body> section are correct",
     },
     {
         questions: "How to write an IF statement in JavaScript?",
-        choicesEl: ["if (i == 5)", "if i = 5", "if i = 5 then", "if i == 5 then"],
-        answer: "if (i == 5)"
+        choices: ["if (i == 5)", "if i = 5", "if i = 5 then", "if i == 5 then"],
+        answer: "if (i == 5)",
     },
     {
         questions: "What html tag is NOT included in the HEAD tag?",
-        choicesEl: ["link", "meta", "title", "header"],
+        choices: ["link", "meta", "title", "header"],
         answer: "header",
     },
     {
         questions: "How can you add a comment in a JavaScript?",
-        choicesEl: ["//This is a comment", "'This is a comment", "<!--This is a comment-->"],
+        choices: ["//This is a comment", "'This is a comment", "<!--This is a comment-->"],
         answer: "//This is a comment",
     }
-]
+];
